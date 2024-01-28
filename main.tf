373f023792147b807685194b730e047f374ac83d
SabryMo <15993660+SabryMo@users.noreply.github.com>
2024-01-28 16:19:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,7 @@ provider "aws" {
   # access_key = ""        //you can hard-coding credintial here, but isn't recommended and risky. 
   # secret_key = ""
 
-  //Better to configure access throgh aws configure profile (cli/vscode extention)
-  //Or
 
-  //env varibles (Below steps)
-  // for Linux ...
-  //export AWS_ACCESS_KEY_ID="anaccesskey"
-  //export AWS_SECRET_ACCESS_KEY="asecretkey"
-  //export AWS_REGION="us-west-2"
-  // for windows ...
-  //$env:AWS_ACCESS_KEY_ID="anaccesskey"
-  //$env:AWS_SECRET_ACCESS_KEY="asecretkey"
-  //$env:AWS_REGION="us-west-2"
 }
 
 //AWS EC2 Instance
